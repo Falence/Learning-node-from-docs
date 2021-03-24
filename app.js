dafbdceab89949879e3f0a3e307b8253004c1b0f
Falence Lemungoh <falencelemungoh@gmail.com>
2021-03-24 14:17:13 +0100

    Using and its dot method:error, log, trace, time, timeEnd etc

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/*
 const http = require('http');
 
 const hostname = '127.0.0.1';
@@ -12,6 +13,53 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}`);
 });
+
+
+
+*/
+
+const x = 1
+const y = 2
+const z = 3
+console.count(
+    'The value of x is ' + x +
+    ' and has been checked .. how many times?'
+)
+console.count(
+    'The value of x is ' + x +
+    ' and has been checked .. how many times?'
+)
+console.count(
+    'The value of y is ' + y +
+    ' and has been checked .. how many times?'
+)
+
+const oranges = ['orange', 'orange']
+const apples = ['just one apple']
+
+oranges.forEach(fruit => {
+    console.count(fruit)
+})
+
+const doSomething = () => console.log('test')
+const measureDoingSomething = () => {
+    console.time('doSomething()')
+    //do something, and measure the time it takes
+    doSomething()
+    console.timeEnd('doSomething()')
+}
+measureDoingSomething()
+
+
+
+
+
+
+
+
+
+
+
 
 
 
